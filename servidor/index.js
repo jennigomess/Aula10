dcebc22173baae4625ef55ff90edaf4d16c521a0
jennigomess <jennifergomesoliveira.15@gmail.com>
2024-11-22 10:49:58 -0300

    jj
    22/11

---
+++ servidor/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2');
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'aulabd'
+});
+
+db.connect((erro => {
+    if (erro) {
+        console.error('Erro ao conectar ao MySQl:', erro);
+    } else {
+        console.log('Conectado ao MySQl com sucesso!');
+    }
+}));
